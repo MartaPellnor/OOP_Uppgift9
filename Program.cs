3591b74dd1da83081eeb82a2f0fe00f64dfab10c
MartaPellnor <pellnormarta@gmail.com>
2021-01-26 17:01:40 +0100

    Overall structure, PlayerTurn()
    Added overall structure, coded PlayerTurn()

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace OOP_Uppgift9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Rock, Paper, Scissors game!");
+            
+            Console.WriteLine("ROUND 1");
+            string playerInput = PlayerTurn();
+            string computerInput = ComputerTurn();
+
+
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+
+        static string PlayerTurn()
+        {
+            string playerInput;
+
+            do {
+                Console.WriteLine("Will you choose Rock (r), Paper (p) or Scissors (s)?");
+                playerInput = Console.ReadLine();
+
+                if(playerInput == "r" || playerInput == "p" || playerInput == "s") {
+
+                }
+                else {
+                    Console.WriteLine("Please write one of the letters 'r', 'p' or 's' in lower case.");
+                }
+            }while(playerInput != "r" || playerInput != "p" || playerInput != "s");
+
+            return playerInput;
+        }
+
+        static string ComputerTurn()
+        {
+            return;
+        }
+
+        static int Score()
+        {
+
+            return;
         }
     }
 }
