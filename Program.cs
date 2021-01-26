9951728a606cc7a9f642f5884a974af52683955e
MartaPellnor <pellnormarta@gmail.com>
2021-01-26 16:37:15 +0100

    Initialized project
    Created .cs files in VSC

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Uppgift9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
